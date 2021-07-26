e585c8be943b82294d622dbdce47503468787b90
ViniciusMarquesp <ciusivinicius@hotmail.com>
2021-07-26 16:25:01 -0300

    Exercicio blog pessoal

---
+++ TesteSpring/src/test/java/br/com/generation/helloteste/TesteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.helloteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
