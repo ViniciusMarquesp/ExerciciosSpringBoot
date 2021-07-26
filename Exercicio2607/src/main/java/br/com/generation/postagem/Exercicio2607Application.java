40666799bacf9d44f8c50b90bd875cf22732206c
ViniciusMarquesp <ciusivinicius@hotmail.com>
2021-07-26 16:32:01 -0300

    teste

--- Exercicio2607/src/main/java/br/com/generation/postagem/Exercicio2607Application.java
+++ Exercicio2607/src/main/java/br/com/generation/postagem/Exercicio2607Application.java
@@ -1,4 +1,4 @@
-package br.com.generation.postagem;
+package br.com.generation.postagem; //asda
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
